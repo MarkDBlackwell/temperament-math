1b50ad0f1c6e1c57a7cb1a246fd1714aed8d42c6
Mark D. Blackwell <markdblackwell01@gmail.com>
2021-01-21 13:22:07 -0500

    Sort

--- calculate.rb
+++ calculate.rb
@@ -1,61 +1,9 @@
-def octave_size
-  12
-end
-
-def third_major_size
-  4
-end
-
-def thirds_combined
-  @thirds_combined ||= begin
-    ring = (octave_size.div third_major_size).times.map do |e|
-      third_major_size * e
-    end
-    third_major_size.times.map do |offset|
-      ring.map do |e|
-        offset + e
-      end
-    end
-  end
-end
-
 def fifth_max
   1
 end
 
 def fifth_min
   -2
-end
-
-def radix
-  @radix ||= begin
-    fifth_max - fifth_min + 1
-  end
-end
-
-def universe_size
-  @universe_size ||= begin
-    radix ** octave_size
-  end
-end
-
-def thirds_match_octave?(thirds)
-  thirds_combined.each do |e|
-    ring = e.map do |i|
-      thirds.at i
-    end
-    return false unless 0 == ring.sum
-  end
-  true
-end
-
-def thirds_build(fifths)
-  octave_size.times.map do |step|
-    third_major_size.times.map do |offset|
-      index = (step - offset) % octave_size
-      fifths.at index
-    end.sum
-  end
 end
 
 def good_find
@@ -75,6 +23,16 @@ def good_find
   end
 end
 
+def octave_size
+  12
+end
+
+def radix
+  @radix ||= begin
+    fifth_max - fifth_min + 1
+  end
+end
+
 def run
   # p thirds_combined
   p "#{fifth_min} #{fifth_max}"
@@ -85,6 +43,48 @@ def run
   [good_length, 10].min.times do |i|
     fifths = good.at i
     p "#{fifths} #{thirds_build fifths}"
+  end
+end
+
+def third_major_size
+  4
+end
+
+def thirds_build(fifths)
+  octave_size.times.map do |step|
+    third_major_size.times.map do |offset|
+      index = (step - offset) % octave_size
+      fifths.at index
+    end.sum
+  end
+end
+
+def thirds_combined
+  @thirds_combined ||= begin
+    ring = (octave_size.div third_major_size).times.map do |e|
+      third_major_size * e
+    end
+    third_major_size.times.map do |offset|
+      ring.map do |e|
+        offset + e
+      end
+    end
+  end
+end
+
+def thirds_match_octave?(thirds)
+  thirds_combined.each do |e|
+    ring = e.map do |i|
+      thirds.at i
+    end
+    return false unless 0 == ring.sum
+  end
+  true
+end
+
+def universe_size
+  @universe_size ||= begin
+    radix ** octave_size
   end
 end
 
