ce534bf66dc47c82352d3a4fea43b4908f7823d3
Mark D. Blackwell <markdblackwell01@gmail.com>
2021-01-30 19:13:49 -0500

    Add test

---
+++ Rakefile
@@ -0,0 +1,31 @@
+# coding: utf-8
+
+=begin
+Copyright (C) 2021 Mark D. Blackwell.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Contact: Mark D. Blackwell
+markdblackwell01@gmail.com
+=end
+
+desc "The default is 'test:test'"
+task default: :'test:test'
+
+namespace :test do
+  desc 'Run test'
+  task :test do
+    ::Kernel.system './test.sh'
+  end
+end
---
+++ test.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# Copyright (C) 2021 Mark D. Blackwell.
+
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# Contact: Mark D. Blackwell
+# markdblackwell01@gmail.com
+
+#--------------
+
+ruby lib/TemperamentMath/calculate.rb
+
+diff out/output-2-2.txt  test/fixture/output-2-2.txt
+
+diff out/output-fifth-accumulated-2-2.txt  test/fixture/output-fifth-accumulated-2-2.txt
+
+diff out/output-fifth-stepwise-2-2.txt  test/fixture/output-fifth-stepwise-2-2.txt
+
+diff out/output-third-raw-2-2.txt  test/fixture/output-third-raw-2-2.txt
+
+diff out/output-fifth-2-2.txt  test/fixture/output-fifth-2-2.txt
+
+diff out/output-fifth-raw-2-2.txt  test/fixture/output-fifth-raw-2-2.txt
+
+diff out/output-third-2-2.txt  test/fixture/output-third-2-2.txt
+
+diff out/output-tuning-2-2.txt  test/fixture/output-tuning-2-2.txt
