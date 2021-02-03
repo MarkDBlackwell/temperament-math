0cedee67c3dd9dc39120962ae84c0926276f7005
Mark D. Blackwell <markdblackwell01@gmail.com>
2021-02-03 16:38:56 -0500

    Refactor build accumulated fifth sets

--- lib/TemperamentMath/calculate.rb
+++ lib/TemperamentMath/calculate.rb
@@ -35,9 +35,8 @@ module TemperamentMath
 
   def fifth_accumulated_sets_build
     out_fifth_raw.rewind
-    out_fifth_raw.each_line do |line|
-    end
-    @@fifth_accumulated_sets = @@fifth_sets.map do |set|
+    @@fifth_accumulated_sets = out_fifth_raw.each_line.map do |line|
+      set = line.split(' ').map &:to_i
       sum = 0
       set.map{|e| sum += e}
     end
